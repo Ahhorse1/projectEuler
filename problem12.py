c925d06785f5860851d3a553639045188d9fa940
JC Yan <ahhorsegan@gmail.com>
2022-09-29 00:00:16 -0700

    Problem 12

--- problem12.py
+++ problem12.py
@@ -1,18 +1,30 @@
-def factorCounter(a):
-    counter = 0
-    for i in range(1,a+1):
-        if a%i == 0:
-            counter = counter + 1
-    return counter
+import math
 
+def factorCounter(num):
+    totalFactors = 0
+    # Check from 1 to Sqrt(Num)
+    for i in range(1,math.floor(math.sqrt(num))):
+        # if i == Sqrt(Num), then only 1 additional factor
+        if num % i == 0:
+            if num/i == i:
+                totalFactors = totalFactors + 1
+            else:
+                totalFactors = totalFactors + 2
+    return totalFactors
 
-foundTriangleNumber = False
-j = 1
-triangleNumber = 0
-while foundTriangleNumber != True:
-    triangleNumber = triangleNumber + j
-    if factorCounter(triangleNumber) >= 500:
-        foundTriangleNumber = True
-    j = j + 1
+def calculateTriangleNumber(n):
+    # Base Case
+    if n == 1:
+        return 1
+    # Gauss Sum
+    return (n*(n+1))/2
 
-print(triangleNumber)
+numFactors = 0
+counter = 1
+triNum = 0
+# Check each triangle number until one with 500 factors is found
+while numFactors < 500:
+    triNum = calculateTriangleNumber(counter)
+    numFactors = factorCounter(triNum)
+    counter = counter + 1
+print(triNum)
